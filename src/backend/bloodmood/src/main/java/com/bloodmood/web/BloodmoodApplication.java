e1c28fc3199c66001bd7eaa373862150ee7daa96
unknown <a0970255137@gmail.com>
2026-01-20 19:57:45 +0800

    add backend

---
+++ src/backend/bloodmood/src/main/java/com/bloodmood/web/BloodmoodApplication.java
@@ -0,0 +1,13 @@
+package com.bloodmood.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodmoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodmoodApplication.class, args);
+	}
+
+}
