e32b1a8d9898ad3d1c12624195ff16ec55f51684
arac1123 <neilhouse124@gmail.com>
2026-02-02 19:23:35 +0800

    add Docker&Docker-compose

---
+++ android/database/Cursor.java
@@ -0,0 +1,8 @@
+package android.database;
+
+/**
+ * Minimal stub of android.database.Cursor to allow non-Android compilation.
+ * This is intentionally empty; expand if you need cursor behaviour in tests.
+ */
+public interface Cursor {
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# BloomMood 植物心情(不知道叫啥)
+## Docker 使用
+- 到\BloomMood這頁
+- 輸入`docker compose up --build`(這是建立)
+- 到 http:/localhost:3000
